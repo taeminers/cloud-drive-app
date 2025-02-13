4f8598867486b751a2545fdd75db22fb013f5e7b
taeminers <taeminers@gmail.com>
2025-02-13 13:16:38 +0900

    feat: basic layout of dashboard

---
+++ constants/index.ts
@@ -0,0 +1,10 @@
+export const navItems = [
+  { name: "Dashboard", icon: "/assets/icons/dashboard.svg", url: "/" },
+  { name: "Documents", icon: "/assets/icons/documents.svg", url: "/documents" },
+  { name: "Images", icon: "/assets/icons/images.svg", url: "/images" },
+  { name: "Media", icon: "/assets/icons/video.svg", url: "/media" },
+  { name: "Others", icon: "/assets/icons/others.svg", url: "/others" },
+];
+
+export const avatarPlaceholderUrl =
+  "https://camo.githubusercontent.com/752f8eb9623a9a431e5485741a36d2f2a9caaeead813b035d57c320458bc482e/68747470733a2f2f6176617461722e6972616e2e6c696172612e72756e2f7075626c6963";
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "camo.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cloud.appwrite.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
