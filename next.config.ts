a5cf2e49d9117988671a2b4a701a95b95a199f27
taeminers <taeminers@gmail.com>
2025-02-16 22:36:47 +0900

    feat: sheet UI in mobile navigation view

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "cloud.appwrite.io",
       },
+      {
+        protocol: "https",
+        hostname: "www.google.com",
+      },
     ],
   },
 };
